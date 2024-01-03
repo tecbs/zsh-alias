853bb6531bc5ab0a80a43e8bf80892a9358b6b40
tecbs <81802421+tecbs@users.noreply.github.com>
2024-01-03 17:46:38 +0000

    Update zsh-alias-order.plugin.zsh

--- zsh-alias-order.plugin.zsh
+++ zsh-alias-order.plugin.zsh
@@ -3,4 +3,11 @@
 alias cp='cp -iv'
 alias mv="mv -iv"
 alias rm="rm -vI"
-alias mkd="mkdir -pv" 
+alias mkd="mkdir -pv"
+alias ls="ls -hN --color=auto --group-directories-first"
+alias agrep="grep --color=auto"
+alias diff="diff --color=auto"
+alias ccat="highlight --out-format=ansi"
+alias ip="ip -color=auto"
+
+
